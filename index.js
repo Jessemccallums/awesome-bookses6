0bbd60faa1f8596c8a6daeb7906f10cbb7d0afda
Jessemccallums <jessemccallums2001@gmail.com>
2022-07-04 08:11:07 -0400

    add changes

---
+++ index.js
@@ -0,0 +1,21 @@
+import Book from './modules/classes.js';
+
+import displayContact from './modules/contact.js';
+
+import displayForm from './modules/form.js';
+
+import displayList from './modules/List.js';
+
+// import displayTime
+
+Book.add();
+
+Book.display();
+
+Book.remove();
+
+displayList();
+
+displayForm();
+
+displayContact();
